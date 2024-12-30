82e7d1a991c5adafe897bae90f12a3ca28894f20
dai0115 <hateen0808@gmail.com>
2024-12-30 12:42:35 +0900

    アプリのデプロイできるまで

---
+++ app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	port := "8080" // Cloud Run ではデフォルトでポート8080が利用されます
+	log.Printf("Starting server on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
